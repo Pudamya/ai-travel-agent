2dd29a4b67ef43447fbfacbec21e9342f4f9b128
Pudamya Vidusini Rathnayake <pudamyavidusinirathnayake@gmail.com>
2026-03-15 16:14:05 +0530

    Add rapidAPI

--- backend/tools/hotelTool.js
+++ backend/tools/hotelTool.js
@@ -1,26 +1,34 @@
 import axios from "axios"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 export async function searchHotels(city){
+
+try{
 
 const res = await axios.get(
 "https://booking-com.p.rapidapi.com/v1/hotels/search",
 {
-headers:{
-"X-RapidAPI-Key":process.env.RAPID_API_KEY
-},
 params:{
 dest_type:"city",
 dest_id:city
+},
+headers:{
+"X-RapidAPI-Key":process.env.RAPIDAPI_KEY,
+"X-RapidAPI-Host":"booking-com.p.rapidapi.com"
 }
 }
 )
 
-return res.data.result.slice(0,5).map(h=>({
+return res.data.result
 
-name:h.hotel_name,
-price:h.min_total_price,
-rating:h.review_score
+}catch(err){
 
-}))
+console.log("Hotel API failed")
+
+return []
+
+}
 
 }
