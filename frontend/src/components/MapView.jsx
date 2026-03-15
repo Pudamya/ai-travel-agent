45178574edea0a2b5c30f6d7e7d3f2ead5358eda
Pudamya Vidusini Rathnayake <pudamyavidusinirathnayake@gmail.com>
2026-03-15 17:01:17 +0530

    Icon fix

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -3,11 +3,11 @@ import "leaflet/dist/leaflet.css"
 
 export default function MapView({places}){
 
-const center=[7.8731,80.7718] // Sri Lanka default
+const center=[7.8731,80.7718]
 
 return(
 
-<div style={{height:"400px",marginTop:"20px"}}>
+<div style={{height:"400px"}}>
 
 <MapContainer center={center} zoom={6} style={{height:"100%"}}>
 
@@ -16,9 +16,13 @@ url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 />
 
 {places?.map((p,i)=>(
+
 <Marker key={i} position={[p.lat,p.lng]}>
+
 <Popup>{p.name}</Popup>
+
 </Marker>
+
 ))}
 
 </MapContainer>
