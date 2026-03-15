c2b3772fabc5ec514dfb973fe79f3cb5e021d25c
Pudamya Vidusini Rathnayake <pudamyavidusinirathnayake@gmail.com>
2026-03-15 16:44:35 +0530

    Added error handling

--- backend/tools/pricePredictionTool.js
+++ backend/tools/pricePredictionTool.js
@@ -2,11 +2,21 @@ import axios from "axios"
 
 export async function predictFlightPrice(data){
 
+try{
+
 const res = await axios.post(
 "http://localhost:5000/predict",
 data
 )
 
 return res.data.predicted_price
+
+}catch(err){
+
+console.log("ML price prediction failed")
+
+return Math.floor(Math.random()*400)+100
+
+}
 
 }
