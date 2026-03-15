510ba895ea4db4ce560129f9597e35f01beb2104
Pudamya Vidusini Rathnayake <pudamyavidusinirathnayake@gmail.com>
2026-03-15 17:19:06 +0530

    Removing route

--- frontend/src/components/FlightCard.jsx
+++ frontend/src/components/FlightCard.jsx
@@ -1,23 +1,23 @@
-export default function FlightCard({ flight }) {
+export default function FlightCard({flight}){
 
-return (
+return(
 
 <div className="card flightCard">
 
 <h3>{flight.airline}</h3>
 
-<p><strong>Flight:</strong> {flight.flight}</p>
+<p>Flight: {flight.flight}</p>
 
-<p><strong>Route:</strong> {flight.departure} → {flight.arrival}</p>
+<p>
+{flight.departure} → {flight.arrival}
+</p>
 
-<p><strong>Price:</strong> ${flight.price}</p>
+<p>Price: ${flight.price}</p>
 
-<p><strong>Predicted Price:</strong> ${flight.predicted_price}</p>
+<p>Predicted: ${flight.predicted_price}</p>
 
 <p className="recommend">
-
 {flight.recommendation}
-
 </p>
 
 </div>
