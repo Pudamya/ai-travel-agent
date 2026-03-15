1eb25ae6a444f478506fa716a2a292f5b8841d47
Pudamya Vidusini Rathnayake <pudamyavidusinirathnayake@gmail.com>
2026-03-15 17:02:10 +0530

    Adding rating

--- frontend/src/components/ImageGallery.jsx
+++ frontend/src/components/ImageGallery.jsx
@@ -2,11 +2,13 @@ export default function HotelCard({hotel}){
 
 return(
 
-<div className="card">
+<div className="card hotel">
 
 <h3>{hotel.name}</h3>
 
 <p>Price per night: ${hotel.price}</p>
+
+<p>Rating: {hotel.rating || "4.2"}</p>
 
 </div>
 
