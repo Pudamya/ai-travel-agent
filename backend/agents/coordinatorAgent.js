34de592982756b4bf386aff916edb30e1371066b
Pudamya Vidusini Rathnayake <pudamyavidusinirathnayake@gmail.com>
2026-03-15 16:15:50 +0530

    Update the coordinator agent by adding log

--- backend/agents/coordinatorAgent.js
+++ backend/agents/coordinatorAgent.js
@@ -6,79 +6,63 @@ import { generateTravelImage } from "../tools/imageTool.js"
 import { generatePlan } from "./plannerAgent.js"
 import { predictFlightPrice } from "../tools/pricePredictionTool.js"
 
-
 export async function runTravelPlanner(data){
+
+console.log("Planner input:",data)
 
 const weather = await getWeather(data.to)
 
 const places = await searchPlaces(data.to)
 
-const flights = await searchFlights(data.from, data.to)
+const flights = await searchFlights(data.from,data.to)
 
 const hotels = await searchHotels(data.to)
 
 const image = await generateTravelImage(data.to)
 
-
-// Predict price for each flight
 const flightsWithPrediction = await Promise.all(
 
-flights.map(async (flight)=>{
+flights.map(async f=>{
 
-const predictedPrice = await predictFlightPrice({
-
-airline: flight.airline || "Indigo",
-source_city: data.from,
-departure_time: "Morning",
-stops: "zero",
-arrival_time: "Night",
-destination_city: data.to,
-class: "Economy",
-duration: flight.duration || 120,
-days_left: 20
-
+const predicted = await predictFlightPrice({
+airline:"Indigo",
+source_city:data.from,
+departure_time:"Morning",
+stops:"zero",
+arrival_time:"Night",
+destination_city:data.to,
+class:"Economy",
+duration:120,
+days_left:20
 })
 
-let recommendation = "Price stable"
-
-if(predictedPrice > flight.price){
-recommendation = "Book now — price likely to increase"
-}
-
-if(predictedPrice < flight.price){
-recommendation = "Wait — price may decrease"
-}
-
-return {
-...flight,
-predicted_price: predictedPrice,
-recommendation
+return{
+...f,
+predicted_price:predicted,
+recommendation:
+predicted > f.price
+? "Book now"
+: "Price stable"
 }
 
 })
 
 )
 
-
-// Generate AI travel plan
 const plan = await generatePlan({
 weather,
 places,
-flights: flightsWithPrediction,
-hotels,
-image
+flights:flightsWithPrediction,
+hotels
 })
 
-
-return {
-
+return{
 weather,
 places,
-flights: flightsWithPrediction,
+flights:flightsWithPrediction,
 hotels,
 image,
 plan
-
 }
 
 }
